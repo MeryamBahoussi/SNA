0265b715d14a789815a0a60c68e6a5c92f9b45d6
Meryam <116731707+MeryamBahoussi@users.noreply.github.com>
2023-05-02 14:46:27 +0200

    Update streamlit_app.py
    part 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,14 @@
+import streamlit as st
+from azure.storage.blob import BlobServiceClient
+import pandas as pd
+from io import BytesIO
+import pickle
+from bertopic import BERTopic
+from os import path
 
-import streamlit 
 
-streamlit.title('TEST') 
+#############################################
+# Setup Streamlit config
+#############################################
+st.set_page_config(page_title = "SNA-TM", layout='wide')
+st.sidebar.markdown("SNA-TM")
