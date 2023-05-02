481ba1ea9c797b148ca0544050e5aec9cff50e5b
Meryam <116731707+MeryamBahoussi@users.noreply.github.com>
2023-05-02 14:55:08 +0200

    Update streamlit_app.py
    add

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,12 @@ import pickle
 #############################################
 st.set_page_config(page_title = "SNA-TM", layout='wide')
 st.sidebar.markdown("SNA-TM")
+
+
+
+#############################################
+# Setup variables for Azure Data lake
+#############################################
+storageAccount = "sasnadatalake"
+containerName = "sna"
+accessKey = "JyC+4No6iP37g5K0BfGtN9UoihS3Hn8uND/R1wc1CNclAmkMPcSCk2FTbJybreIhUgVKTJdN+0kC+ASt2A4WNg=="
