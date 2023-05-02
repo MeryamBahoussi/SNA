a1bd0624021cf8cbd9d6175b3e070e1c88d47628
Meryam <116731707+MeryamBahoussi@users.noreply.github.com>
2023-05-02 16:40:43 +0200

    Update streamlit_app.py
    azure

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,3 +13,20 @@ from os import path
 #############################################
 st.set_page_config(page_title = "SNA-TM", layout='wide')
 st.sidebar.markdown("SNA-TM")
+
+
+
+#############################################
+# Setup variables for Azure Data lake
+#############################################
+storageAccount = "sasnadatalake"
+containerName = "sna"
+accessKey = "JyC+4No6iP37g5K0BfGtN9UoihS3Hn8uND/R1wc1CNclAmkMPcSCk2FTbJybreIhUgVKTJdN+0kC+ASt2A4WNg=="
+
+
+#############################################
+# Setup connection to Azure Data lake
+#############################################
+connectionString = f"DefaultEndpointsProtocol=https;AccountName={storageAccount};AccountKey={accessKey};EndpointSuffix=core.windows.net"
+blob_service_client = BlobServiceClient.from_connection_string(connectionString)
+container_client = blob_service_client.get_container_client(containerName)
