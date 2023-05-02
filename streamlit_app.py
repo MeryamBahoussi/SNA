ea2f9fead2dff3082b9b674c5204525c312288c5
Meryam <116731707+MeryamBahoussi@users.noreply.github.com>
2023-05-02 16:38:29 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,5 +9,5 @@ from azure.storage.blob import BlobServiceClient
 #############################################
 # Setup Streamlit config
 #############################################
-st.set_page_config(page_title = "SNA-TMMM", layout='wide')
-st.sidebar.markdown("SNA-TM")
+st.set_page_config(page_title = "SNA-TM", layout='wide')
+st.sidebar.markdown("SNA-TMMM")
